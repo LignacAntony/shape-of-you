d7a45590062cf122b37874afbca7c3d7cfe52125
Oceloott <baptiste.blst@gmail.com>
2025-02-16 20:22:45 +0100

    fix front

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,50 @@
 
 namespace App\Controller;
 
+use App\Repository\OutfitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-// #[IsGranted('ROLE_USER')]
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function __invoke(): Response
+    #[IsGranted('ROLE_USER')]
+    public function __invoke(Request $request, OutfitRepository $outfitRepository): Response
     {
-        return $this->render('index.html.twig', []);
+        $sortBy = $request->query->get('sort_by', 'recent');
+        $search = $request->query->get('search', '');
+
+        $orderByField = match ($sortBy) {
+            'recent' => 'o.createdAt',
+            'oldest' => 'o.createdAt',
+            'most_likes' => 'o.likesCount',
+            'least_likes' => 'o.likesCount',
+            default => 'o.createdAt',
+        };
+        $orderDirection = in_array($sortBy, ['oldest', 'least_likes']) ? 'ASC' : 'DESC';
+
+        $queryBuilder = $outfitRepository->createQueryBuilder('o')
+            ->where('o.isPublished = :published')
+            ->setParameter('published', true);
+
+        if (!empty($search)) {
+            $queryBuilder->andWhere('o.name LIKE :search OR o.description LIKE :search')
+                ->setParameter('search', "%$search%");
+        }
+
+        $outfits = $queryBuilder
+            ->orderBy($orderByField, $orderDirection)
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult();
+
+        return $this->render('index.html.twig', [
+            'outfits' => $outfits,
+            'sort_by' => $sortBy,
+            'search' => $search
+        ]);
     }
 }
